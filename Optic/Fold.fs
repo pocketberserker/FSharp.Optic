a735e2319319dfb37b153b7b29b8448e3e71fb68
pocketberserker <pocketberserker@gmail.com>
2015-05-29 01:57:14 +0900

    Fold

---
+++ Optic/Fold.fs
@@ -0,0 +1,7 @@
+﻿namespace Optic
+
+[<AbstractClass>]
+type Fold<'S, 'A> =
+  abstract member FoldMap: #Monoid<'M> * ('A -> 'M) -> ('S -> 'M)
+  member this.Fold(m: #Monoid<'A>): ('S -> 'A) =
+    this.FoldMap(m, id)
---
+++ Optic/Monoid.fs
@@ -0,0 +1,8 @@
+﻿namespace Optic
+
+type Semigroup<'F> =
+  abstract member Append: 'F * 'F -> 'F
+
+type Monoid<'F> =
+  inherit Semigroup<'F>
+  abstract member Zero: unit -> 'F
