ef75882d02ef77eb8a0312a6ee1de85eafd92b72
pocketberserker <pocketberserker@gmail.com>
2015-05-30 00:33:19 +0900

    PPrism, Prism

---
+++ Optic/PPrism.fs
@@ -0,0 +1,6 @@
+﻿namespace Optic
+
+type PPrism<'S, 'T, 'A, 'B> =
+  abstract member GetOrModify: 'S -> Choice<'T, 'A>
+  abstract member ReverseGet: 'B -> 'T
+  abstract member GetOption: 'S -> 'A option
---
+++ Optic/Prism.fs
@@ -0,0 +1,10 @@
+﻿namespace Optic
+
+type Prism<'S, 'A> = {
+  PPrism: PPrism<'S, 'S, 'A, 'A>
+}
+with
+  interface PPrism<'S, 'S, 'A, 'A> with
+    member this.GetOrModify(s) = this.PPrism.GetOrModify(s)
+    member this.ReverseGet(a) = this.PPrism.ReverseGet(a)
+    member this.GetOption(s) = this.PPrism.GetOption(s)
