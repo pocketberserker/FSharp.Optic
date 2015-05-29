3df660a31590bb8e6680411d8bcf9327b9a1cec9
pocketberserker <pocketberserker@gmail.com>
2015-05-30 00:58:45 +0900

    PTraversal, Traversal

---
+++ Optic/PTraversal.fs
@@ -0,0 +1,9 @@
+﻿namespace Optic
+
+type PTraversal<'S, 'T, 'A, 'B> =
+  abstract member ModifyFunctionF: ('A -> ('C -> 'B)) -> ('S -> ('C -> 'T))
+  abstract member ModifyChoiceF: ('A -> Choice<'L, 'B>) -> ('S -> Choice<'L, 'T>)
+  abstract member ModifyTrampolineF: ('A -> Trampoline<'B>) -> ('S -> Trampoline<'T>)
+  abstract member ModifyListF: ('A -> 'B list) -> ('S -> 'T list)
+  abstract member ModifyOptionF: ('A -> 'B option) -> ('S -> 'T option)
+  abstract member FoldMap: Monoid<'M> * ('A -> 'M) -> ('S -> 'M)
---
+++ Optic/Traversal.fs
@@ -0,0 +1,13 @@
+﻿namespace Optic
+
+type Traversal<'S, 'A> = {
+  PTraversal: PTraversal<'S, 'S, 'A, 'A>
+}
+with
+  interface PTraversal<'S, 'S, 'A, 'A> with
+    member this.ModifyTrampolineF(f) = this.PTraversal.ModifyTrampolineF(f)
+    member this.ModifyOptionF(f) = this.PTraversal.ModifyOptionF(f)
+    member this.ModifyListF(f) = this.PTraversal.ModifyListF(f)
+    member this.ModifyFunctionF(f) = this.PTraversal.ModifyFunctionF(f)
+    member this.ModifyChoiceF(f) = this.PTraversal.ModifyChoiceF(f)
+    member this.FoldMap(m, f) = this.PTraversal.FoldMap(m, f)
