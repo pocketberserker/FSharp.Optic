649e47b34debce68f4c0442d4f510279e0549a15
pocketberserker <pocketberserker@gmail.com>
2015-05-30 00:17:45 +0900

    PLens, Lens

---
+++ Optic/Lens.fs
@@ -0,0 +1,15 @@
+﻿namespace Optic
+
+type Lens<'S, 'A> = {
+  PLens: PLens<'S, 'S, 'A, 'A>
+}
+with
+  interface PLens<'S, 'S, 'A, 'A> with
+    member this.Get(s) = this.PLens.Get(s)
+    member this.Set(a) = this.PLens.Set(a)
+    member this.ModifyTrampolineF(f) = this.PLens.ModifyTrampolineF(f)
+    member this.ModifyOptionF(f) = this.PLens.ModifyOptionF(f)
+    member this.ModifyListF(f) = this.PLens.ModifyListF(f)
+    member this.ModifyFunctionF(f) = this.PLens.ModifyFunctionF(f)
+    member this.ModifyChoiceF(f) = this.PLens.ModifyChoiceF(f)
+    member this.Modify(f) = this.PLens.Modify(f)
---
+++ Optic/PLens.fs
@@ -0,0 +1,11 @@
+﻿namespace Optic
+
+type PLens<'S, 'T, 'A, 'B> =
+  abstract member Get: 'S -> 'A
+  abstract member Set: 'B -> ('S -> 'T)
+  abstract member ModifyFunctionF: ('A -> ('C -> 'B)) -> ('S -> ('C -> 'T))
+  abstract member ModifyChoiceF: ('A -> Choice<'L, 'B>) -> ('S -> Choice<'L, 'T>)
+  abstract member ModifyTrampolineF: ('A -> Trampoline<'B>) -> ('S -> Trampoline<'T>)
+  abstract member ModifyListF: ('A -> 'B list) -> ('S -> 'T list)
+  abstract member ModifyOptionF: ('A -> 'B option) -> ('S -> 'T option)
+  abstract member Modify: ('A -> 'B) -> ('S -> 'T)
