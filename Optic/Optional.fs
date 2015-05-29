e8e6ea8d14689927731f41f3b04c02b7f381c1ca
pocketberserker <pocketberserker@gmail.com>
2015-05-29 23:46:06 +0900

    POptional, Optional

---
+++ Optic/Optional.fs
@@ -0,0 +1,16 @@
+﻿namespace Optic
+
+type Optional<'S, 'A> = {
+  POptional: POptional<'S, 'S, 'A, 'A>
+}
+with
+  interface POptional<'S, 'S, 'A, 'A> with
+    member this.Set(a) = this.POptional.Set(a)
+    member this.ModifyTrampolineF(f) = this.POptional.ModifyTrampolineF(f)
+    member this.ModifyOptionF(f) = this.POptional.ModifyOptionF(f)
+    member this.ModifyListF(f) = this.POptional.ModifyListF(f)
+    member this.ModifyFunctionF(f) = this.POptional.ModifyFunctionF(f)
+    member this.ModifyChoiceF(f) = this.POptional.ModifyChoiceF(f)
+    member this.Modify(f) = this.POptional.Modify(f)
+    member this.GetOrModify(s) = this.POptional.GetOrModify(s)
+    member this.GetOption(s) = this.POptional.GetOption(s)
---
+++ Optic/POptional.fs
@@ -0,0 +1,12 @@
+﻿namespace Optic
+
+type POptional<'S, 'T, 'A, 'B> =
+  abstract member GetOrModify: 'S -> Choice<'T, 'A>
+  abstract member Set: 'B -> ('S -> 'T)
+  abstract member GetOption: 'S -> 'A option
+  abstract member ModifyFunctionF: ('A -> ('C ->'B)) -> ('S -> ('C -> 'T))
+  abstract member ModifyChoiceF: ('A -> Choice<'L, 'B>) -> ('S -> Choice<'L, 'T>)
+  abstract member ModifyTrampolineF: ('A -> Trampoline<'B>) -> ('S -> Trampoline<'T>)
+  abstract member ModifyListF: ('A -> 'B list) -> ('S -> 'T list)
+  abstract member ModifyOptionF: ('A -> 'B option) -> ('S -> 'T option)
+  abstract member Modify: ('A -> 'B) -> ('S -> 'T)
