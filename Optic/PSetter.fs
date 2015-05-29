4620859b54d7e2a72fc1485ab08d9a74bbb9bcac
pocketberserker <pocketberserker@gmail.com>
2015-05-30 00:44:58 +0900

    PSetter, Setter

---
+++ Optic/PSetter.fs
@@ -0,0 +1,5 @@
+﻿namespace Optic
+
+type PSetter<'S, 'T, 'A, 'B> =
+  abstract member Modify: ('A -> 'B) -> ('S -> 'T)
+  abstract member Set: 'B -> ('S -> 'T)
---
+++ Optic/Setter.fs
@@ -0,0 +1,9 @@
+﻿namespace Optic
+
+type Setter<'S, 'A> = {
+  PSetter: PSetter<'S, 'S, 'A, 'A>
+}
+with
+  interface PSetter<'S, 'S, 'A, 'A> with
+    member this.Modify(f) = this.PSetter.Modify(f)
+    member this.Set(a) = this.PSetter.Set(a)
