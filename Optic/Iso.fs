8f76aff94e554e62c0e13addcee51a045bfe8610
pocketberserker <pocketberserker@gmail.com>
2015-05-30 01:11:30 +0900

    PIso, Iso

---
+++ Optic/Iso.fs
@@ -0,0 +1,10 @@
+﻿namespace Optic
+
+type Iso<'S, 'A> = {
+  PIso: PIso<'S, 'S, 'A, 'A>
+}
+with
+  interface PIso<'S, 'S, 'A, 'A> with
+    member this.Get(s) = this.PIso.Get(s)
+    member this.ReverseGet(a) = this.PIso.ReverseGet(a)
+    member this.Reverse() = this.PIso.Reverse()
---
+++ Optic/PIso.fs
@@ -0,0 +1,6 @@
+﻿namespace Optic
+
+type PIso<'S, 'T, 'A, 'B> =
+  abstract member Get: 'S -> 'A
+  abstract member ReverseGet: 'B -> 'T
+  abstract member Reverse: unit -> PIso<'B, 'A, 'T, 'S>
